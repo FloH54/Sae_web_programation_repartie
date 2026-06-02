ded85f6bfea9b083c9a1d52c9d6b0751c0f6dad7
MathisWISPELAERE <mathis.wispelaere7@etu.univ-lorraine.fr>
2026-06-02 11:49:18 +0200

    ajout des nouvelles tables dans la BDD

--- creation_db.sql
+++ creation_db.sql
@@ -37,3 +37,41 @@ CREATE TABLE IF NOT EXISTS `reservation` (
         ON DELETE CASCADE
         ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `tables` (
+    `id_table` INT AUTO_INCREMENT PRIMARY KEY,
+    `nb_place` INT NOT NULL,
+    `id_restaurant` INT NOT NULL,
+    CONSTRAINT `fk_table_restaurant`
+        FOREIGN KEY (`id_restaurant`)
+        REFERENCES `restaurants` (`id`)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+); ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `commande` (
+    `id_com` INT AUTO_INCREMENT PRIMARY KEY,
+    `date_com` DATE NOT NULL,
+    `nb_pers` INT NOT NULL,
+    `date_paie` DATE NOT NULL,
+    `montant` DECIMAL(6,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `contient` (
+    `id_com` INT NOT NULL,
+    `id_plat` INT NOT NULL,
+
+    PRIMARY KEY (`id_com`, `id_plat`),
+
+    CONSTRAINT `fk_contient_commande`FOREIGN KEY (`id_com`) REFERENCES `commande` (`id_com`) ON DELETE CASCADE ON UPDATE CASCADE,
+
+    CONSTRAINT `fk_contient_plat` FOREIGN KEY (`id_plat`) REFERENCES `plat` (`id_plat`) ON DELETE CASCADE ON UPDATE CASCADE
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `plat` (
+    `id_plat` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `libelle` TEXT NOT NULL,
+    `prix_unite` DECIMAL(6,2),
+    `qte_stock` INT NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
